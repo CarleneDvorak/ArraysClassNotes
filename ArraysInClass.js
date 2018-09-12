fa4cba941ee465a7025533be313b507478c27c91
CarleneDvorak <42741569+CarleneDvorak@users.noreply.github.com>
2018-09-11 19:21:02 -0500

    it works now

--- ArraysInClass.js
+++ ArraysInClass.js
@@ -8,7 +8,7 @@ console.log(movies.length);
 console.log(movies[i]);
 
 //iteratign over an array
-for (i=0; i< movies.length; i++){
+for ( var i=0; i< movies.length; i++){
     console.log (movies[i]);}
     
 //using the for-of method
